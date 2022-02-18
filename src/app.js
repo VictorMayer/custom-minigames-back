9fc5e199a5547f9c37fe9a673b0e4b007695970e
victorMayer <victor.mayer@outlook.com>
2022-02-18 00:01:49 -0300

    branch offtopic

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import faker from 'faker-br';
 import serverError from './middlewares/serverError.js';
 
 import * as userController from './controllers/userController.js';
@@ -12,6 +13,16 @@ app.use(cors());
 // STATUS CHECK
 app.post('/health', async (req, res) => {
     res.send('OK!');
+});
+
+app.get('/fakes', async (req, res) => {
+    const result = {
+        name: faker.name.firstName(),
+        email: faker.internet.email(),
+        phone: faker.phone.phoneNumber(),
+    };
+    console.log(result);
+    return res.send(result);
 });
 
 // ROUTES
